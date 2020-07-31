b5eca8b68b0055652c8fd4b5a3e0bae87fcd831f
Sean Dez <sd@seandezoysa.com>
2020-08-01 02:57:14 +0700

    setup basic menu

---
+++ .eslintrs.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    // ESLint doesn't understand these for Typescript
+    // TSC will throw on errors though
+    'import/named': 0, // off
+    'import/no-unrseolved': 0,
+    'import/extensions': 0,
+    'no-undef': 0,
+    'no-unused-vars': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+};
---
+++ src/navigation/NavMenu.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import StyledLink from "../shared/StyledLink";
+
+interface DataObject { displayName: string, slug: string }
+
+const linkData: Array<DataObject> = [
+  { displayName: 'Phone Numbers', slug: 'numbers' },
+  { displayName: 'Settings', slug: 'settings' },
+];
+
+interface PropsShape {}
+
+export default (props: PropsShape) => {
+
+  return (
+    <header>
+      <AppBar position="static">
+        <Toolbar>
+          <HorizontalList>
+            { linkData.map((dataItem: DataObject) => (
+              <StyledLink to={dataItem.slug}>{dataItem.displayName}</StyledLink>
+            )) }
+          </HorizontalList>
+        </Toolbar>
+      </AppBar>
+    </header>
+  )
+} 
+
+const HorizontalList = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  list-style-type: none;
+  border: 4px dotted pink;
+  width: 90vw;
+  max-width: 1200px;
+
+  & > * {
+    text-decoration: none;
+  }
+`;
